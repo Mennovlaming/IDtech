5ae2bc64da2fa700bbfa226d94974f92ab19b975
Menno Vlaming <mennovlaming@gmail.com>
2023-11-29 16:27:07 +0100

    deploy error

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 import '/style.css';
-import * as barchart from '/scripts/barchart.js';
-import * as dataloader from '/scripts/dataloader.js';
-import * as piechart from '/scripts/piechart.js';
+import * as barchart from './scripts/barchart.js';
+import * as dataloader from './scripts/dataloader.js';
+import * as piechart from './scripts/piechart.js';
 
 /* Default exports per components are a ✨ little prettier here */
 
 
-console.log('Hello Tech Track 7');
+console.log('Hello Tech Track 8');
--- scripts/dataloader.js
+++ scripts/dataloader.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Fetch data from data.json
-  fetch("../data.json")
+  fetch("./data.json")
     .then(response => response.json())
     .then(data => {
       // Update the HTML content for each section
