685a8354140473607fd409b98973e5c60ba59b32
Menno Vlaming <mennovlaming@gmail.com>
2023-11-29 15:57:49 +0100

    deploy

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import './style.css';
-import * as barchart from './scripts/barchart.js';
-import * as dataloader from './scripts/dataloader.js';
-import * as piechart from './scripts/piechart.js';
+import '/style.css';
+import * as barchart from '/scripts/barchart.js';
+import * as dataloader from '/scripts/dataloader.js';
+import * as piechart from '/scripts/piechart.js';
 
 /* Default exports per components are a ✨ little prettier here */
 
